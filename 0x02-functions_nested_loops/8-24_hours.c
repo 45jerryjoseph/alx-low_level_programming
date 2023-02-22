eab0403365667a5364cfb71fc15143ae9bfcd403
45jerryjoseph <45jerryjoseph@gmail.com>
2023-02-21 23:57:29 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * jack_baver - prints every minute of the day of jack Bauer
+ * jack_bauer - prints every minute of the day of jack Bauer
  *
- * Return: The time
+ * Return: time interval
  */
 void jack_bauer(void)
 {
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if((a <= 1 && b <=9) || (a <= 2 && b <= 3))
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
 				for (c = 0; c <= 5; c++)
 				{
