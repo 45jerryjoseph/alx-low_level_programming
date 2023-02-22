578d9d596c8af4e6c71339511141176d1a9494ca
45jerryjoseph <45jerryjoseph@gmail.com>
2023-02-21 19:16:44 -0800

    absolute valua tobe printed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+int _abs(int);
+
+/**
+ * _abs -used to compute absolute value of Integer.
+ * @n: is the number to compute.
+ *
+ * Return: Absolute value of number of zero
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int abs;
+		abs = n * -1;
+		return (abs);
+	}
+	return (n);
+}
