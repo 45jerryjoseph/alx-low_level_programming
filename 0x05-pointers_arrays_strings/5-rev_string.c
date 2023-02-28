e838be83fcf687b97a8fee8a2b3035bc42d5bd94
45jerryjoseph <45jerryjoseph@gmail.com>
2023-02-28 12:56:18 -0800

    message

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,11 +2,11 @@
 
 
 /**
-  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ *
+ * Return: void
+ */
 void rev_string(char *s)
 {
 	int i, c, k;
