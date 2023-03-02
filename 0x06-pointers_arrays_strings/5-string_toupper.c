fc79e83bd45b9560a8cd195b9babf4781cdfd899
45jerryjoseph <45jerryjoseph@gmail.com>
2023-03-02 01:38:24 -0800

    changes 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,16 @@
  */
 char *string_toupper(char *str)
 {
-	for (int i = 0; str[i] != 0; i++)
+	int i =  0;
+
+	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32
 		}
+		
+		i++;
 	}
 	return (str);
 }
