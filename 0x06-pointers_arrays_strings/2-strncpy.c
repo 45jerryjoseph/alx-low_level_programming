46d6c94c53bff320d9a5ba6fcc9133e9f65d6b36
45jerryjoseph <45jerryjoseph@gmail.com>
2023-03-02 06:25:21 -0800

    change

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * _strncpy - copy a string
- * @dest: The destination
- * @src: The source 
- * @n: The limit set
- *
- * Return: Value
- */
+  * _strncpy - Copy a string
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The copy limit
+  *
+  * Return: char value
+  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
