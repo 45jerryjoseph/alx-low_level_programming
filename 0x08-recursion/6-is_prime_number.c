13182e68872a1048c4b9415939c1d8ccfe30f62c
45jerryjoseph <45jerryjoseph@gmail.com>
2023-03-08 16:30:15 -0800

    changes made

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,7 +16,13 @@ int check(int a, int b)
 		return (check(a + 1, b));
 }
 
-
+/**
+ * is_prime_number - tells us if an integer is a prime number or not
+ *
+ * @n: the number to check
+ *
+ * Return: 0 if the number is not prime, and 1 if it is
+ */
 int is_prime_number(int n)
 {
 	if (n == 2)
