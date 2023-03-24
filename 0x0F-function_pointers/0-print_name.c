c593c3d353bca1f6fc6ae3c9dccb83985f0cf6f7
45jerryjoseph <45jerryjoseph@gmail.com>
2023-03-24 04:50:44 -0700

    change 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: the name to print
@@ -9,5 +10,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
 		return;
-		f(name);
+	f(name);
 }
